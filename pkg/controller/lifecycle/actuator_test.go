3ce66c6ac6f5007a0183c422c55081ac7e94dbf5
Rafael Franzke <rafael.franzke@sap.com>
2023-05-10 16:44:28 +0200

    Adapt Prometheus custom pod label selectors

--- pkg/controller/lifecycle/actuator_test.go
+++ pkg/controller/lifecycle/actuator_test.go
@@ -638,6 +638,7 @@ func expectedSeedService(namespace string) string {
 kind: Service
 metadata:
   annotations:
+    networking.resources.gardener.cloud/from-all-scrape-targets-allowed-ports: '[{"protocol":"TCP","port":8080}]'
     networking.resources.gardener.cloud/from-policy-allowed-ports: '[{"protocol":"TCP","port":8080}]'
     networking.resources.gardener.cloud/from-policy-pod-label-selector: all-scrape-targets
   creationTimestamp: null
