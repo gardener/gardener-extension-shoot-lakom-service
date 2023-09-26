48f59f74f4982743a2ac2708d6c548516ceafdda
Shafeeque E S <shafeeque.e.s@sap.com>
2023-09-26 18:40:27 +0530

    Adapt tests

--- pkg/admission/http_test.go
+++ pkg/admission/http_test.go
@@ -176,7 +176,7 @@ var _ = Describe("Admission Webhooks", func() {
 				Log: logger.WithName("server"),
 			}
 
-			expected := fmt.Sprintf(`{%s,"response":{"uid":"","allowed":true,"status":{"metadata":{},"reason":%q,"code":200}}}
+			expected := fmt.Sprintf(`{%s,"response":{"uid":"","allowed":true,"status":{"metadata":{},"message":%q,"code":200}}}
 `, gvkJSONv1, value)
 
 			ctx, cancel := context.WithCancel(context.WithValue(context.Background(), key, value))
@@ -207,7 +207,7 @@ var _ = Describe("Admission Webhooks", func() {
 				Log: logger.WithName("server"),
 			}
 
-			expected := fmt.Sprintf(`{%s,"response":{"uid":"","allowed":true,"status":{"metadata":{},"reason":%q,"code":200}}}
+			expected := fmt.Sprintf(`{%s,"response":{"uid":"","allowed":true,"status":{"metadata":{},"message":%q,"code":200}}}
 `, gvkJSONv1, "application/json")
 
 			ctx, cancel := context.WithCancel(context.Background())
