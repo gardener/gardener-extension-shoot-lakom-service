gardener/gardener-extension-shoot-lakom-service

commit da9e764eed7e170c833310252924eace66fa2e3b

Mitigate wrongly returned NoMatchingSignature error when context is canceled (#28)
* Mitigate wrongly returned NoMatchingSignature error when context is canceled

* Use errors.Is instead of checking for substring

Co-authored-by: Dimitar Mirchev <dimitar.mirchev@sap.com>

---------

Co-authored-by: Dimitar Mirchev <dimitar.mirchev@sap.com>

pkg/lakom/verifysignature/verifier.go
@@ -87,6 +87,12 @@ func verify(ctx context.Context, imageRef name.Reference, keys []crypto.PublicKe
 			}
 
 			if IsNoMatchingSignature(err) {
+				if errors.Is(err, context.Canceled) {
+					// Mitigation for https://github.com/gardener/gardener-extension-shoot-lakom-service/issues/25
+					// TODO(vpnachev): remove when https://github.com/sigstore/cosign/issues/3133 is fixed and vendored
+					return false, err
+				}
+
 				log.Info("no matching signatures found for current public key", "error", err.Error())
 				continue
 			}

pkg/lakom/verifysignature/verifier_test.go
@@ -85,10 +85,8 @@ IqozONbbdbqz11hlRJy9c7SG+hdcFl9jE9uE/dwtuwU2MqU9T/cN0YkWww==
 
 		It("Should fail image verification when context is canceled", func() {
 			canceledCtx, cancel := context.WithCancel(ctx)
-			go func() {
-				time.Sleep(time.Millisecond * 10)
-				cancel()
-			}()
+			cancel()
+
 			verified, err := directVerifier.Verify(canceledCtx, "eu.gcr.io/gardener-project/gardener/apiserver@sha256:249ea7f1d0439a94893b486e7820f6f0ab52522c5f22e1bad21782d6381e739e", kcr)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("context canceled"))
@@ -156,10 +154,7 @@ IqozONbbdbqz11hlRJy9c7SG+hdcFl9jE9uE/dwtuwU2MqU9T/cN0YkWww==
 
 		It("Should not cache failed image verification when context is canceled", func() {
 			canceledCtx, cancel := context.WithCancel(ctx)
-			go func() {
-				time.Sleep(time.Millisecond * 10)
-				cancel()
-			}()
+			cancel()
 
 			image := "eu.gcr.io/gardener-project/gardener/apiserver@sha256:249ea7f1d0439a94893b486e7820f6f0ab52522c5f22e1bad21782d6381e739e"
 			verified, err := cachedVerifier.Verify(canceledCtx, image, kcr)

commit e800ad6a85a350fe864486356b8ed40c34fa2e5a

Fix mitigation for NoMatchingSignature error
The check introduced with #28 did not work

Co-authored-by: vpnachev <vladimir.nachev@sap.com>
Co-authored-by: ialidzhikov <i.alidjikov@gmail.com>

pkg/lakom/verifysignature/verifier.go
@@ -87,7 +87,7 @@ func verify(ctx context.Context, imageRef name.Reference, keys []crypto.PublicKe
 			}
 
 			if IsNoMatchingSignature(err) {
-				if errors.Is(err, context.Canceled) {
+				if errors.Is(ctx.Err(), context.Canceled) || errors.Is(ctx.Err(), context.DeadlineExceeded) {
 					// Mitigation for https://github.com/gardener/gardener-extension-shoot-lakom-service/issues/25
 					// TODO(vpnachev): remove when https://github.com/sigstore/cosign/issues/3133 is fixed and vendored
 					return false, err