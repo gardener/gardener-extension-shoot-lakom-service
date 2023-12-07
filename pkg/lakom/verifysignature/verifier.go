db2679933a2d53d576fd921bfa399a60e789e638
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-07 14:05:48 +0200

    Fix mitigation for NoMatchingSignature error
    The check introduced with #28 did not work
    
    Co-authored-by: vpnachev <vladimir.nachev@sap.com>
    Co-authored-by: ialidzhikov <i.alidjikov@gmail.com>

--- pkg/lakom/verifysignature/verifier.go
+++ pkg/lakom/verifysignature/verifier.go
@@ -87,7 +87,7 @@ func verify(ctx context.Context, imageRef name.Reference, keys []crypto.PublicKe
 			}
 
 			if IsNoMatchingSignature(err) {
-				if errors.Is(err, context.Canceled) {
+				if errors.Is(ctx.Err(), context.Canceled) || errors.Is(ctx.Err(), context.DeadlineExceeded) {
 					// Mitigation for https://github.com/gardener/gardener-extension-shoot-lakom-service/issues/25
 					// TODO(vpnachev): remove when https://github.com/sigstore/cosign/issues/3133 is fixed and vendored
 					return false, err
