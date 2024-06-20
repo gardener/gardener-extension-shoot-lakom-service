bea20525b1a1983665f8479f1c740a4a2d6c8b89
Radoslav Hubenov <rrhubenov@gmail.com>
2024-06-20 11:33:00 +0300

    Fix cosign pub key & add skaffold env var comments

--- hack/skaffold-imagevector.sh
+++ hack/skaffold-imagevector.sh
@@ -1,3 +1,14 @@
+# Ideally we'd use $SKAFFOLD_IMAGE_REPO & $SKAFFOLD_IMAGE_TAG instead of
+# extracting these values from $SKAFFOLD_IMAGE, but for some reason,
+# the value for $SKAFFOLD_IMAGE_REPO is everything before the '/' symbol, instead
+# of everything before the last ':' symbol (where the image tag should start)
+# e.g. For SKAFFOLD_IMAGE=localhost:5001/lakom:tag
+# SKAFFOLD_IMAGE_REPO=localhost:5001 (instead of localhost:5001/lakom)
+# SKAFFOLD_IMAGE_TAG=tag
+#
+# This might need to be brought up as an issue on skaffold's side. For anyone
+# interested, the logic for the parsing of the image name in the skaffold project 
+# is contained in https://github.com/GoogleContainerTools/skaffold/blob/main/pkg/skaffold/docker/reference.go
 IMAGE_REPO=$(echo $SKAFFOLD_IMAGE | cut -d':' -f1,2)
 IMAGE_TAG=$(echo $SKAFFOLD_IMAGE | cut -d':' -f3)
 
