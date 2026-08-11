822ff43e35e1ce972a42336c561974339c53b623
Hristo Mavrodiev <42392154+h-mavrodiev@users.noreply.github.com>
2026-08-11 18:17:13 +0300

    Add e2e test make command and script placeholders (#336)
    * add e2e test make command and script placeholder
    
    * add license header
    
    * address review comments

---
+++ hack/ci-e2e-kind.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+echo "e2e tests placeholder..."
